9c1d7e00674bc33564a74546a985111a2aed558f
prathamahuja <prathamahuja@localhost>
2022-09-06 14:04:53 +0530

    Hiii

--- src/package1/BubbleSort.java
+++ src/package1/BubbleSort.java
@@ -32,7 +32,7 @@ public class BubbleSort {
    for(int i=0; i < arr.length; i++){  
            System.out.print(arr[i] + " ");  
    }  
-
+System.out.println();
 }  
 		// TODO Auto-generated method stub
     
